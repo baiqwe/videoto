fcc5e3213b0d2bb73e09b55155649e8ce1e3cbe9
baiqwe <764894226@qq.com>
2025-12-15 22:49:03 +0800

    style: update footer credit to 'built by Bai'

--- components/footer.tsx
+++ components/footer.tsx
@@ -39,12 +39,12 @@ export function Footer() {
         <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row md:py-0">
           <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
             <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-              Built by{" "}
+              built by{" "}
               <Link
-                href="https://Raphael.app"
+                href="#"
                 className="font-medium underline underline-offset-4"
               >
-                Raphael Starter
+                Bai
               </Link>
             </p>
           </div>
@@ -85,11 +85,10 @@ export function Footer() {
         </div>
         <div className="mt-16 flex flex-col items-center justify-between gap-4 border-t pt-8 md:flex-row">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built by{" "}
+            built by{" "}
             <Link href="#" className="font-medium underline underline-offset-4">
-              Raphael Starter
+              Bai
             </Link>
-            .
           </p>
         </div>
       </div>
